c6bd55d05a9bdb6569b912762b7305ab0af0c55f
atheer waled <atheerwaled66@hotmail.se>
2019-10-31 18:24:31 +0100

    Initial Commit

---
+++ MusicX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicX
+//
+//  Created by Atheer on 2019-10-31.
+//  Copyright © 2019 Atheer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
