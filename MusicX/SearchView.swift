6edfc14f8908f4aeb357226d96a2d83acbb0ac21
atheer waled <atheerwaled66@hotmail.se>
2019-11-09 11:52:12 +0100

    Added a song card view to the search view very basic animations, will begin work on the network side and start retrieving song information from genius API

--- MusicX/SearchView.swift
+++ MusicX/SearchView.swift
@@ -9,31 +9,29 @@
 import SwiftUI
 
 struct SearchView: View {
+    // a state for when search button is tapped
     @State private var isSearching: Bool = false
+    // this variable is used to update the view
+    // when user enters text into textfiled
     @State private var searchMusic: String = ""
     
     var body: some View {
         NavigationView {
             VStack {
                 HStack {
-                    searchTextField(isSearching: $isSearching, searchMusic: $searchMusic)
+                    searchTextField(isSearching: isSearching, searchMusic: $searchMusic)
                 }
-                VStack(alignment: .leading, spacing: 5.0) {
-                    Text("Press the search button to begin searching for a song")
-                    HStack {
-                        Text("Example:")
-                        Text("Sia - Chandelier")
-                            .font(.headline)
-                            .fontWeight(.heavy)
-                    }
-                }
-                .padding(.horizontal, 8)
-                .offset(y: UIScreen.main.bounds.height/3)
-            }
-            
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: Alignment.top)
-            .navigationBarTitle(Text("Search"))
+                exampleText(isSearching: isSearching)
                 
+                SongCardView(isSearching: isSearching)
+                
+            }
+            // makings sure navigation view takes the whole space
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: Alignment.top)
+            // adding navigation bar title
+            .navigationBarTitle(Text("Search"))
+            // adding search button with that toggles the
+            // text field
             .navigationBarItems(trailing:
                 Button(action: {
                     // when search button is tapped
@@ -41,12 +39,10 @@ struct SearchView: View {
                 }) {
                     searchButton()
             })
+        // added background color for whole view
         .background(Color(UIColor.systemGray5).edgesIgnoringSafeArea(.all))
             
         }
-        
-        
-        
     }
 }
 
@@ -68,9 +64,11 @@ struct searchButton: View {
 }
 
 struct searchTextField: View {
-    @Binding var isSearching: Bool
+    var isSearching: Bool
     @Binding var searchMusic: String
     var body: some View {
+        // creating text field and when user commits the text
+        // gets to reset
         TextField("Search", text: $searchMusic , onCommit: {
             self.searchMusic = ""
         })
@@ -85,5 +83,63 @@ struct searchTextField: View {
                 
         
         
+    }
+}
+// this view is used to explain to user on how to begin search
+struct exampleText: View {
+    var isSearching: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5.0) {
+            Text("Press the search button to begin searching for a song")
+            HStack {
+                Text("Example:")
+                Text("Sia - Chandelier")
+                    .font(.headline)
+                    .fontWeight(.heavy)
+            }
+        }
+        .padding(.horizontal, 8)
+        .opacity(isSearching ? 0 : 1)
+        .offset(y: UIScreen.main.bounds.height/3)
+        .animation(.spring())
+    }
+}
+
+// this is a custom modifier for the song title
+struct songTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.black)
+    }
+}
+
+// the view for song card view
+struct SongCardView: View {
+    var isSearching: Bool
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Image("test")
+                .resizable()
+                .cornerRadius(30)
+                .frame(width: UIScreen.main.bounds.width/1.5, height: UIScreen.main.bounds.width/1.5)
+            HStack {
+                Text("Chandelier")
+                    .modifier(songTitle())
+                Text("-")
+                    .modifier(songTitle())
+                Text("Sia")
+                    .modifier(songTitle())
+                
+            }
+            .background(Color.gray.opacity(0.8))
+            .padding()
+        }
+        // Will add proper animation for teh card view
+        // this is a simple fix
+        .opacity(isSearching ? 1 : 0)
+        .animation(.spring())
     }
 }
